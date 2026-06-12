e4dcb7a2a7f54aba17658c8c02d4755b7b6d066b
minhtq <minhtranquang2197@gmail.com>
2026-06-12 18:33:48 +0700

    first commit

---
+++ src/test/java/com/wazado/authstarter/AuthStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wazado.authstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
